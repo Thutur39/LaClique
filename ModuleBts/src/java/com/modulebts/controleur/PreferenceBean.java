36519c6c5228aac0714647da2d163f164922afaa
Thutur39 <35227104+Thutur39@users.noreply.github.com>
2018-03-13 15:55:41 +0100

    ok

---
+++ ModuleBts/src/java/com/modulebts/controleur/PreferenceBean.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.modulebts.controleur;
+
+import com.modulebts.Preference;
+import com.modulebts.ejb.PreferenceBeanEJB;
+import javax.ejb.EJB;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author drack
+ */
+public class PreferenceBean {
+
+    @EJB
+    private PreferenceBeanEJB ejb;
+
+    @PersistenceContext
+    EntityManager em;
+    
+    private Preference preference = new Preference();
+}
